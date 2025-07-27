3bfe9bc73012b176ddf583f9425cd7ca17ba9cbe
Mario Teik <mario.teik@gmail.com>
2025-07-27 09:37:17 -0300

    adds github actions

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import Unfonts from "unplugin-fonts/vite";
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: process.env.VITE_BASE_PATH || "/",
   build: {
     target: "esnext",
   },
