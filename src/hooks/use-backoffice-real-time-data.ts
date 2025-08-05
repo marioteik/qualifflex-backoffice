5b08b3da93561a38e281fd7889c7ea7bce629ac4
Mario Teik <mario.teik@gmail.com>
2025-08-05 01:09:32 -0300

    fixes

--- src/hooks/use-backoffice-real-time-data.ts
+++ src/hooks/use-backoffice-real-time-data.ts
@@ -23,6 +23,8 @@ export function useBackofficeRealTimeData(
       auth: {
         token: session?.access_token,
       },
+      transports: ["websocket"],
+      path: "/socket.io",
     });
 
     updates.on("connect", () => {
